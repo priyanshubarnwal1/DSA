7c4aaa6fd7a06c121fd164aaed7f2d841c9d5882
priyanshubarnwal1 <impriyanshu0107@gmail.com>
2026-08-19 00:12:42 +0530

    Time: 5 ms (37.53%), Space: 43.3 MB (83.63%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,25 +1,49 @@
+// class Solution {
+//     public String removeOuterParentheses(String s) {
+//           StringBuilder ans = new StringBuilder();
+//         int depth = 0;
+
+//         for (char ch : s.toCharArray()) {
+
+//             if (ch == '(') {
+//                 if (depth > 0) {
+//                     ans.append(ch);
+//                 }
+//                 depth++;
+//             } 
+//             else {
+//                 depth--;
+
+//                 if (depth > 0) {
+//                     ans.append(ch);
+//                 }
+//             }
+//         }
+
+//         return ans.toString();
+//     }
+// }
+
+
+
 class Solution {
     public String removeOuterParentheses(String s) {
-          StringBuilder ans = new StringBuilder();
-        int depth = 0;
+    int cnt = 0;
 
-        for (char ch : s.toCharArray()) {
+    StringBuilder ans = new StringBuilder();
 
-            if (ch == '(') {
-                if (depth > 0) {
-                    ans.append(ch);
-                }
-                depth++;
-            } 
-            else {
-                depth--;
-
-                if (depth > 0) {
-                    ans.append(ch);
-                }
-            }
+    for(int i=0; i<s.length(); i++){
+        if(s.charAt(i) == ')'){
+            cnt--;
         }
+        if(cnt != 0){
+            ans.append(s.charAt(i));
+        }
+        if(s.charAt(i) == '('){
+            cnt++;
+        }
+    }
+    return ans.toString();
 
-        return ans.toString();
     }
 }
